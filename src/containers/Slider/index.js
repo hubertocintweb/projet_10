b39d2126f4c8f3605024d394b6c826a9dff53197
luc <luc.leonardi@opsonet.com>
2023-06-26 12:45:18 +0200

    fix slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,24 +10,31 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
-  const [timeoutId, setTimeoutId] = useState(null);
-  const nextCard = () => {
-    const id = setTimeout(() => {
-      let nextIndex = 0;
-      if (byDateDesc) {
-        nextIndex = index < byDateDesc.length - 1 ? index + 1 : 0;
-      }
-      setIndex(nextIndex);
-    }, 5000);
-    setTimeoutId(id);
-  };
+  const [intervalId, setIntervalId] = useState(null);
+
   const changeCard = (idx) => {
     setIndex(idx);
-    clearTimeout(timeoutId);
-  }
+    // clear auto slider
+    clearInterval(intervalId);
+  };
+
   useEffect(() => {
-    nextCard();
-  });
+    // wait byDateDesc is ready and execute one only time
+    if (byDateDesc && !intervalId) {
+      // use setInterval instead of setTimeOut
+      const id = setInterval(() => {
+        // pass callback function to setIndex to get current index
+        // index var doesn't work because we are inside an interval function
+        setIndex((currentIndex) => {
+          let nextIndex = 0;
+          nextIndex =
+            currentIndex < byDateDesc.length - 1 ? currentIndex + 1 : 0;
+          return nextIndex;
+        });
+      }, 5000);
+      setIntervalId(id);
+    }
+  }, [setIndex, setIntervalId, byDateDesc, index, intervalId]);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
@@ -46,23 +53,24 @@ const Slider = () => {
               </div>
             </div>
           </div>
-          
         </div>
       ))}
       <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc?.map((event2, radioIdx) => (
-                <input
-                  key={`${event2.title}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  readOnly
-                  onClick={() => {changeCard(radioIdx)}}
-                />
-              ))}
-            </div>
-          </div>
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event2, radioIdx) => (
+            <input
+              key={`${event2.title}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              readOnly
+              onClick={() => {
+                changeCard(radioIdx);
+              }}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
